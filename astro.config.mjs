d0d320f51dccbb11e5426ca85449f01648cd1f43
Juan Franco home Desktop <juanferfranco@gmail.com>
2026-07-26 15:06:02 -0500

    Uni2 in beta

--- astro.config.mjs
+++ astro.config.mjs
@@ -35,7 +35,7 @@ export default defineConfig({
 					items: [
 						// Each item here is one entry in the navigation menu.
 						{ label: 'Unidad 1', slug: 'units/unit1' },
-						{ label: 'Unidad 2', slug: 'units/unit2' },
+						{ label: 'Unidad 2', slug: 'units/unit2', badge: 'New'},
 						{ label: 'Unidad 3', slug: 'units/unit3' },
 						{ label: 'Unidad 4', slug: 'units/unit4' },
 						{ label: 'Unidad 5', slug: 'units/unit5' },
